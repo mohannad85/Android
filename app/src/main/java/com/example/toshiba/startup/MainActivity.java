09e430f40450f0f32804a709bbf38a01b3989390
Mohannad <eng.moh205@gmail.com>
2017-04-01 17:20:45 +0200

    GUI changing :)

--- app/src/main/java/com/example/toshiba/startup/MainActivity.java
+++ app/src/main/java/com/example/toshiba/startup/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.toshiba.startup;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void show(View view) {
+        Toast.makeText(this, "You clicked on OK", Toast.LENGTH_SHORT).show();
     }
 }
